bcf3604a476a71bef56368ee11031118ac0e1dc2
3030625 <3030625@30306-25>
2023-03-24 10:21:06 +0900

    333

--- src/main/java/com/example/enigma/repository/SettingEnigmaRepository.java
+++ src/main/java/com/example/enigma/repository/SettingEnigmaRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import com.example.enigma.entry.SettingEnigma;
 
 public interface SettingEnigmaRepository extends CrudRepository<SettingEnigma, Integer>{
+	
+	//33333
 
 }
